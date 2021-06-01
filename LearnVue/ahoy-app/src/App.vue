ed0bb88b758a79b689dadcafbec356658e32435f
Thachamon <5910110602@psu.ac.th>
2021-06-01 11:44:19 +0700

    1st commit : learning with ahoy

---
+++ LearnVue/ahoy-app/src/App.vue
@@ -0,0 +1,55 @@
+<!--<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--    <User name="Chuck Norris" bio="The strongest man in the world" />
+  </div>
+</template>
+-->
+<!-- Slot -->
+<template>
+  <div id="app">
+
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <User name="Chuck Norris">
+    <p>The strongest man in the world!</p>
+  </User>
+  </div>
+</template>
+
+<!--
+<script>
+ import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+-->
+<script>
+import User from './components/User.vue'
+
+export default {
+  name: 'app',
+  components: {
+    User
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ LearnVue/ahoy-app/src/components/User.vue
@@ -0,0 +1,27 @@
+<!-- Probs.bio -->
+<!--
+<template>
+  <div class="user">
+    <h2>{{ name }}</h2>
+    <p>{{ bio }}</p>
+  </div>
+</template>
+-->
+<!-- Slot -->
+<template>
+  <div class="user">
+    <h2>{{ name }}</h2>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'User',
+    props: {
+      name: String,
+      bio: String
+    }
+  }
+</script>
+<style></style>
